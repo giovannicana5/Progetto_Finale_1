74db6ccff6cfe980bb34cdb6323d1d98237345c7
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-02-24 18:25:58 +0100

    User Story 7 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\ResizeImage;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
@@ -33,6 +35,8 @@ class CreateArticleForm extends Component
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
                 // $this->article->images()->create(['path' => $image->store('images', 'public')]);
                 dispatch(new ResizeImage($newImage->path, 300, 300));
+                dispatch(new GoogleVisionSafeSearch($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
             }
             File::deleteDirectory(storage_path('/app/livewire/tmp'));
         }
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -25,4 +25,7 @@ class Image extends Model
     public function getUrl($w = null, $h = null) {
         return self::getUrlByFilePath($this->path, $w, $h);
     }
+    protected function casts(): array {
+        return ['labels' => 'array'];
+    }
 }
