e2f3d53388e90f989a9f0b1a9ca14d9c147f75a9
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-02-24 13:17:03 +0100

    User Story 6 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -2,8 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Jobs\ResizeImage;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Livewire\WithFileUploads;
@@ -27,8 +29,12 @@ class CreateArticleForm extends Component
         $this->article = Article::create(['title' => $this->title, 'description' => $this->description, 'price' => $this->price, 'category_id' => $this->category, 'user_id' => Auth::id()]);
         if(count($this->images) > 0) {
             foreach($this->images as $image) {
-                $this->article->images()->create(['path' => $image->store('images', 'public')]);
+                $newFileName = "articles/{$this->article->id}";
+                $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
+                // $this->article->images()->create(['path' => $image->store('images', 'public')]);
+                dispatch(new ResizeImage($newImage->path, 300, 300));
             }
+            File::deleteDirectory(storage_path('/app/livewire/tmp'));
         }
         session()->flash('success', 'Articolo creato correttamente');
         $this->cleanForm();
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -16,8 +16,8 @@
         @if ($article_to_check)
         @if($article_to_check->images->count())
             @foreach($article_to_check->images as $key => $image)
-                <div class="col-6 col-md-4 mb-4">
-                    <img src="{{Storage::url($image->path)}}" class="img-fluid rounded shadow" alt="Immagine {{$key + 1}} dell'articolo {{$article_to_check->title}}">
+                <div class="col-6 col-md-4 mb-4 text-center">
+                    <img src="{{$image->getUrl(300, 300)}}" class="img-fluid rounded shadow" alt="Immagine {{$key + 1}} dell'articolo {{$article_to_check->title}}">
                 </div>
             @endforeach
         @else
