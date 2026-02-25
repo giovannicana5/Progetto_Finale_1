b60e5902d3bae74d72cf349f1a074e4a3a407082
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-02-25 09:51:34 +0100

    User Story 9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Image;
 
 class ResizeImage implements ShouldQueue
@@ -31,6 +32,6 @@ class ResizeImage implements ShouldQueue
         $h = $this->h;
         $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
-        Image::load($srcPath)->crop($w, $h, CropPosition::Center)->save($destPath);
+        Image::load($srcPath)->crop($w, $h, CropPosition::Center)->watermark(base_path('resources/img/watermark.png'), width: 50, height: 50, paddingX: 5, paddingY: 5, paddingUnit: Unit::Percent)->save($destPath);
     }
 }
