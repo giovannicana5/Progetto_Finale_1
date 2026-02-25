9464958de1dfc78f10c292a120f2937d93432c09
Ioan Petrica Canalos <giovanni.canalos@gmail.com>
2026-02-25 09:29:53 +0100

    User Story 8 completata

--- app/Livewire/CreateArticleForm.php
+++ app/Livewire/CreateArticleForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
@@ -34,9 +35,10 @@ class CreateArticleForm extends Component
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
                 // $this->article->images()->create(['path' => $image->store('images', 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                // dispatch(new ResizeImage($newImage->path, 300, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([new ResizeImage($newImage->path, 300, 300), new GoogleVisionSafeSearch($newImage->id), new GoogleVisionLabelImage($newImage->id)])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire/tmp'));
         }
